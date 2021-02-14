571bb69856b6110e5d0d235dd92ece9c6cd4864f
Mathias Nyman <m.nyman@iki.fi>
2021-02-14 20:29:40 +0200

    Fetch datasets removed from vayla from traficom
    Traficom took ownership of hosting some of the datasets previously
    hosted by Vayla. More details can be found on the github issue:
    https://github.com/vokkim/finnish-nautical-chart-vectors/issues/13

--- import/fetch-csv-from-vayla.sh
+++ import/fetch-csv-from-vayla.sh
@@ -4,17 +4,20 @@ set -e
 # APIs:
 # - https://julkinen.vayla.fi/inspirepalvelu/rajoitettu/wfs?request=GetCapabilities
 # - https://julkinen.vayla.fi/inspirepalvelu/avoin/wfs?request=GetCapabilities
+# - https://julkinen.traficom.fi/inspirepalvelu/rajoitettu/wms?request=getcapabilities
+# - https://julkinen.traficom.fi/inspirepalvelu/avoin/wms?request=getcapabilities
 # Data documentation: https://vayla.fi/documents/20473/38174/Merikartta-aineistojen_tietosis%C3%A4ll%C3%B6n_kuvaus.pdf/78afa9e5-8f7c-4430-b798-f9848c79123f
+# Overview documentation: https://www.paikkatietohakemisto.fi/geonetwork/srv/fin/catalog.search#/metadata/1d1c8600-76bf-4e1f-bd09-b5c154ca30dc
 
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $PARENT_PATH
 cd ..
 
-curl "https://julkinen.vayla.fi/inspirepalvelu/rajoitettu/wfs?request=GetFeature&outputFormat=csv&typeNames=rajoitettu:syvyyskayra_v&srsName=urn:ogc:def:crs:EPSG::4326" -o data/syvyyskayra.csv
+curl "https://julkinen.traficom.fi/inspirepalvelu/rajoitettu/wfs?request=GetFeature&outputFormat=csv&typeNames=rajoitettu:DepthContour_L&srsName=urn:ogc:def:crs:EPSG::4326" -o data/syvyyskayra.csv
 
-curl "https://julkinen.vayla.fi/inspirepalvelu/rajoitettu/wfs?request=GetFeature&outputFormat=csv&typeNames=rajoitettu:syvyysalue_a&srsName=urn:ogc:def:crs:EPSG::4326" -o data/syvyysalue.csv
+curl "https://julkinen.traficom.fi/inspirepalvelu/rajoitettu/wfs?request=GetFeature&outputFormat=csv&typeNames=rajoitettu:DepthArea_A&srsName=urn:ogc:def:crs:EPSG::4326" -o data/syvyysalue.csv
 
-curl "https://julkinen.vayla.fi/inspirepalvelu/rajoitettu/wfs?request=GetFeature&outputFormat=csv&typeNames=rajoitettu:syvyyspiste_p&srsName=urn:ogc:def:crs:EPSG::4326" -o data/syvyyspiste.csv
+curl "https://julkinen.traficom.fi/inspirepalvelu/rajoitettu/wfs?request=GetFeature&outputFormat=csv&typeNames=rajoitettu:Sounding_P&srsName=urn:ogc:def:crs:EPSG::4326" -o data/syvyyspiste.csv
 
 curl "https://julkinen.vayla.fi/inspirepalvelu/avoin/wfs?request=GetFeature&outputFormat=csv&typeNames=avoin:vaylaalueet&srsName=urn:ogc:def:crs:EPSG::4326" -o data/vaylaalueet.csv
 
@@ -28,7 +31,7 @@ curl "https://julkinen.vayla.fi/inspirepalvelu/avoin/wfs?request=GetFeature&outp
 
 curl "https://julkinen.vayla.fi/inspirepalvelu/avoin/wfs?request=GetFeature&outputFormat=csv&typeNames=avoin:navigointilinjat&srsName=urn:ogc:def:crs:EPSG::4326" -o data/navigointilinjat.csv
 
-curl "https://julkinen.vayla.fi/inspirepalvelu/avoin/wfs?request=GetFeature&outputFormat=csv&typeNames=avoin:rantarakenteet_v&srsName=urn:ogc:def:crs:EPSG::4326" -o data/rantarakenteet.csv
+curl "https://julkinen.traficom.fi/inspirepalvelu/avoin/wfs?request=GetFeature&outputFormat=csv&typeNames=avoin:ShorelineConstruction_L&srsName=urn:ogc:def:crs:EPSG::4326" -o data/rantarakenteet.csv
 
 curl "https://julkinen.vayla.fi/inspirepalvelu/avoin/wfs?request=GetFeature&outputFormat=csv&typeNames=avoin:tutkamajakat&srsName=urn:ogc:def:crs:EPSG::4326" -o data/tutkamajakat.csv
 
